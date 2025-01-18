c5fda742e6792bad3420b87441341a388a5ebf72
minh Nguyen <quangminh.nguyen8989@gmail.com>
2025-01-18 17:05:38 -0600

    Declared Variables + Print result

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        //Declare Variables
+        String firstName = "Minh";
+        String lastName = "Nguyen";
+        String streetAddress = "123 Main Street";
+        String city = "Houston";
+        int zipCode = 77095;
+
+        //Print the result
+        System.out.println("My first name is " + firstName);
+        System.out.println("My last name is " + lastName);
+        System.out.println("My address: " + streetAddress);
+        System.out.println("City: " + city);
+        System.out.println("Zipcode: " + zipCode);
     }
 }
